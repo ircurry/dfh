0807e43213c8625a546a6fd65b747cce65ceda98
Ian Curran <icurran@protonmail.com>
2024-07-15 21:30:17 -0400

    dfh: monitor command

--- cmd/dfh/main.go
+++ cmd/dfh/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
+)
+
+const (
+	_ = iota
+	ReadFileFailure
+	MonitorConfigParseFailure
+	MonitorStateFailure
 )
 
 type Monitor struct {
@@ -19,8 +28,16 @@ type Monitor struct {
 };
 
 func (mon *Monitor) String() string {
+	val, err := json.Marshal(mon)
+	if err != nil {
+		panic(err)
+	}
+	return string(val)
+}
+
+func (mon *Monitor) EnableString() string {
 	hyprstring := fmt.Sprintf("%s,%dx%d,%dx%d@%d,%d",
-		mon.Name, mon.Height, mon.Width, mon.X, mon.Y, mon.RefreshRate, mon.Scale)
+		mon.Name, mon.Width, mon.Height, mon.X, mon.Y, mon.RefreshRate, mon.Scale)
 	return hyprstring
 }
 
@@ -29,6 +46,55 @@ func (mon *Monitor) DisableString() string {
 	return hyprstring
 }
 
+type MonitorList []Monitor
+
+func (monlist *MonitorList) fromJson(byt []byte) error {
+	return json.Unmarshal(byt, monlist);
+}
+
+func (monlist *MonitorList) stateStrings(state string) ([]string, error) {
+	strlist := make([]string, 0)
+	enablelist := make([]string, 0)
+	disablelist := make([]string, 0)
+	var stateErr error = nil
+	containsState := false
+	for _, mon := range *monlist {
+		if mon.State == state {
+			enablelist = append(enablelist, mon.EnableString())
+			containsState = true
+		} else {
+			disablelist = append(disablelist, mon.DisableString())
+		}
+	}
+
+	for _, str := range enablelist {
+		strlist = append(strlist, str)
+	}
+	for _, str := range disablelist {
+		strlist = append(strlist, str)
+	}
+
+	if !containsState {
+		stateErr = fmt.Errorf("this monitor configuration does not contain state: %s", state)
+	}
+
+	return strlist, stateErr
+}
+
+func readMonitorConfigFile(file string) ([]byte, error) {
+	switch file {
+	case "":
+		configDir, err := os.UserConfigDir()
+		if err != nil {
+			panic(err)
+		}
+		fileContents, errFile := os.ReadFile(configDir + "/dfh/monitors.json")
+		return fileContents, errFile
+	default:
+		fileContents, err := os.ReadFile(file)
+		return fileContents, err
+	}
+}
 
 func listenEvents(con *net.Conn, lim int, ch chan string) {
     b := make([]byte, 1)
@@ -106,9 +172,48 @@ func main() {
 	lsnCmd := flag.NewFlagSet("spawn", flag.ExitOnError)
 	// TODO: Document Negative numbers being the same as 0
 	lsnNum := lsnCmd.Int("n", 0, "a zero or positive number for how many events to listen to and print")
+	monsCmd := flag.NewFlagSet("monitors", flag.ExitOnError)
+	monsNum := monsCmd.String("f", "", "the json file to read monitor definitions from")
 
 	if (len(os.Args) > 1) {
 		switch os.Args[1] {
+		case "mons":
+			// hyprCtlSock := getHyprCtlSocket()
+			monsCmd.Parse(os.Args[2:])
+			state := monsCmd.Arg(0)
+			if state == "" {
+				fmt.Fprintln(os.Stderr, "No state given")
+				os.Exit(MonitorStateFailure)
+			}
+			contents, err := readMonitorConfigFile(*monsNum)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Unable to read file.")
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(ReadFileFailure)
+			}
+			var monl MonitorList
+			err = monl.fromJson(contents)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Something went wrong parsing config file")
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(MonitorConfigParseFailure)
+			}
+			stateStrings, err := monl.stateStrings(state)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error creating hyprland monitor settings")
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(MonitorStateFailure)
+			}
+			for _, str := range stateStrings {
+				// TODO: make this work with just IPC
+				cmd := exec.Command("hyprctl", "keyword", "monitor", str)
+				if output, err := cmd.Output(); err != nil {
+					panic(err)
+				} else {
+					fmt.Print(string(output))
+				}
+			}
+			return
 		case "dir":
 			fmt.Printf("Hyprland dir: %s\n", getHyprDir())
 			return
