590835064d02f411985f28e53bb759d73fc901a7
Ian Curran <icurran@protonmail.com>
2024-08-24 12:53:01 -0400

    hyprdock: added flags to check if monitors are conneced and for dpms

--- cmd/hyprdock/main.go
+++ cmd/hyprdock/main.go
@@ -14,17 +14,14 @@ func main() {
 	monsCmd := flag.NewFlagSet("monitors", flag.ExitOnError)
 	monsFile := monsCmd.String("f", "", "the json file to read monitor definitions from")
 	monsAllowUnconnected := monsCmd.Bool("a", false, "allow unconnected monitors to be configured")
+	monsCheckConnectedMonitors := monsCmd.Bool("c", false, "check if all monitors in configuration are connected and return error status if not")
+	monsDpms := monsCmd.String("dpms", "disable-off", "what to do about dpms")
 
 
 	if len(os.Args) <= 1 {
 		cli.Die("Not enough arguments", cli.ArgumentError)
 	}
 	monsCmd.Parse(os.Args[1:])
-	state := monsCmd.Arg(0)
-	if state == "" {
-		cli.Die("No state given", cli.MonitorStateFailure)
-	}
-
 	contents, err := monitors.ReadMonitorConfigFile(*monsFile)
 	cli.DieIfErr("Unable to read file.", err, cli.ReadFileFailure)
 
@@ -32,8 +29,6 @@ func main() {
 	err = monl.FromJson(contents)
 	cli.DieIfErr("Something went wrong parsing config file",
 		err, cli.MonitorConfigParseFailure)
-	stateStrings, err := ipc.StateStrings(monl, state)
-	cli.DieIfErr("Error creating hyprland monitor settings", err, cli.MonitorStateFailure)
 
 	wlrdata, err := ipc.WlrRandrJson()
 	cli.DieIfErr("Something went wrong requesting monitor information", err, cli.CommandExecutionError)
@@ -42,10 +37,49 @@ func main() {
 	allMonsPresent, err := monitors.CompareMonitorLists(monl, monitorNames)
 	cli.DieIfErr("Monitor name not found", err, cli.MonitorConfigParseFailure)
 
+	if *monsCheckConnectedMonitors {
+		if allMonsPresent {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+	
 	if !(allMonsPresent || *monsAllowUnconnected) {
 		monsCmd.Usage()
 		cli.Die("Not all monitors in config are present. Consider using the flag -a",
 			cli.InfoRetrevalFailure)
+	}
+
+	state := monsCmd.Arg(0)
+	if state == "" {
+		cli.Die("No state given", cli.MonitorStateFailure)
+	}
+	stateStrings, err := ipc.StateStrings(monl, state)
+	cli.DieIfErr("Error creating hyprland monitor settings", err, cli.MonitorStateFailure)
+
+	switch *monsDpms {
+	case "disable-off":
+		for _, mon := range monl {
+			if (mon.State != state) {
+				fmt.Printf("dpms off %s\n", mon.Name)
+				if output, err := ipc.HyprctlExecCommand("dispatch", "dpms", "off", mon.Name); err != nil {
+					cli.DieErr("something went wrong executing hyprctl", err, cli.CommandExecutionError)
+				} else {
+					fmt.Print(string(output))
+				}
+			}
+		}
+	case "on":
+		fmt.Println("dpms on")
+		if output, err := ipc.HyprctlExecCommand("dispatch", "dpms", "on"); err != nil {
+			cli.DieErr("something went wrong executing hyprctl", err, cli.CommandExecutionError)
+		} else {
+			fmt.Print(string(output))
+		}
+	case "nothing":
+	default:
+		cli.Die("Unknown argument for dpms", cli.ArgumentError)
 	}
 
 	for _, str := range stateStrings {
